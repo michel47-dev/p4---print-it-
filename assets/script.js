db446879a1cce45ed27b10d2d646e2e1d89e772a
Guestin michel <michel47390@outlook.fr>
2026-04-28 20:42:18 +0200

    feat:"ajout des bullets et défilement des slides"

--- assets/script.js
+++ assets/script.js
@@ -4,15 +4,15 @@ const slides = [
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
@@ -21,9 +21,43 @@ let arrowleft = document.querySelector(".arrow_left");
 let arrowright = document.querySelector(".arrow_right");
 
 arrowleft.addEventListener("click", function() {
+	currentIndex--;
+	updateSlide();
 	console.log("click gauche");
 });
 
 arrowright.addEventListener("click", function() {
+	currentIndex++;
+	updateSlide();
 	console.log("click droite");
 });
+
+let dots = document.querySelector(".dots");
+
+for (let i = 0; i < slides.length; i++) {
+	let dot = document.createElement("span");
+	dot.classList.add("dot");
+	dots.appendChild(dot);
+
+	if (i === 0) {
+		dot.classList.add("dot_selected");
+	}
+	dot.classList.add("dot");
+}
+let currentIndex = 0;
+
+let bannerImage = document.querySelector(".banner-img");
+let bannerText = document.querySelector("#banner p");
+
+function updateSlide() {
+  bannerImage.src = slides[currentIndex].image;
+  bannerText.innerHTML = slides[currentIndex].tagLine;
+    let allDots = document.querySelectorAll(".dot");
+
+  for (let i = 0; i < allDots.length; i++) {
+    allDots[i].classList.remove("dot_selected");
+  }
+
+  allDots[currentIndex].classList.add("dot_selected");
+
+}
